22437e9f287a319f239382d7b89eccef8d06c975
David <david@norlab.xyz>
2026-02-23 13:16:35 -0600

    Add files via upload

--- portal.js
+++ portal.js
@@ -1,5 +1,5 @@
 
-const EQ_JSON_URL = 'https://script.google.com/macros/s/AKfycbwIQz0xdojwGVP2J8-se1Ctjl6FeUshP7BnjAm0FkVZu9E3VdpvMWS0binkfQ0oQ3ht/exec';
+const EQ_JSON_URL = 'https://script.google.com/macros/s/AKfycbyyZhFL4W8KbkDvORgbe0fDrx0R1zUvOj_3wFeP_hxDRTfeMT-Pi-HdmPQg7oX7NytV/exec';
 const EQ_FALLBACK = [
   {id:1,  n:"BA88A",   m:"Mindray", c:"Química Clínica", p:39990,   co:30000,  mr:null, pl:["fin"],             mx:6,  dm:0, mc:0,  nota:"0% de interés hasta 4 meses · máximo 6 meses"},
   {id:2,  n:"AQ-200i", m:"Meril",   c:"Química Clínica", p:184338,  co:131670, mr:0.40, pl:["fin","com"],       mx:24, dm:3, mc:48, nota:null},
